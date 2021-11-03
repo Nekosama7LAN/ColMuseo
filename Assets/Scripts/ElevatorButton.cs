8f868b0122366f002d6ade4d8554d5dfd7ec3ee9
Alejandro Jimenez <alejo9512009@hotmail.com>
2021-11-03 17:51:26 -0500

    Prueba museo nuevo

--- Assets/Scripts/ElevatorButton.cs
+++ Assets/Scripts/ElevatorButton.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using System;
 
 public class ElevatorButton : MonoBehaviour
 {
+    public event Action OnElevator;
     [SerializeField] Elevator elevator;
+
     private void OnMouseDown()
     {
         elevator.counter = 0;
