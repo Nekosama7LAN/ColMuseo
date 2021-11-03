f5e7041ebb08eb2954a87dd4f14a76568a0046cd
feliper29 <felipehhs.fh@gmail.com>
2021-11-02 21:49:19 -0500

    Organización
    scrip de las esmeraldas y las esferas de la fuente

---
+++ Assets/Scripts/DiamondMove.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class DiamondMove : MonoBehaviour
+{
+    private Vector3 Initpos;
+    [SerializeField] Vector3 DestPos;
+    private Vector3 Finalpos;
+
+    float counter = 0;
+    float counterDown = 5;
+
+    public float speed = 0;
+    void Update()
+    {
+        RotationOfObject();
+        UpAndDownObject();
+    }
+
+    private void UpAndDownObject()
+    {
+        if (counter < 5f)
+        {
+            Initpos = transform.position;
+            Finalpos = Initpos + DestPos * Time.deltaTime * speed;
+            transform.position = Finalpos;
+            counter = counter + 1 * Time.deltaTime;
+            counterDown = 5f;
+        }
+        else
+        {
+            Initpos = transform.position;
+            Finalpos = Initpos - DestPos * Time.deltaTime * speed;
+            transform.position = Finalpos;
+            counterDown = counterDown - 1 * Time.deltaTime;
+            if (counterDown < 0)
+            {
+                counter = 0;
+            }
+        }
+        print(counter + "counter subida");
+        print(counterDown + "Bajada");
+    }
+    private void RotationOfObject()
+    {
+        transform.Rotate(new Vector3(0f, 100f, 0f) * Time.deltaTime);
+    }
+}
